d7bf74515c7aa0a4931d0da2982d792c3c7a9228
SHIBSUNDAR CHAKRABORTY <shibsundarchakra9@gmail.com>
2025-06-03 19:53:37 +0530

    1AT _BARE_MINIMUM_WORKS_FOR_NOW

---
+++ script.js
@@ -0,0 +1,32 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  // for (i = 0; i < dots.length; i++) {
+  //   dots[i].className = dots[i].className.replace(" active", "");
+  // }
+  slides[slideIndex-1].style.display = "block";  
+  // dots[slideIndex-1].className += " active";
+}
+// ... (rest of the JavaScript code)
+
+// Automatic slideshow
+setInterval(function() {
+  plusSlides(1);
+}, 3000);  // Change image every 3 seconds (adjust as needed)
